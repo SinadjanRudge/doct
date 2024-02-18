5f5abe731c0170743b4d8b849e44132fa7396fb7
Revya <revya25@gmail.com>
2024-02-18 20:02:15 +0800

    Prepared Record View Tabs for Patient

--- app/src/main/java/com/triadss/doctrack2/activity/patient/fragment/RecordFragment.java
+++ app/src/main/java/com/triadss/doctrack2/activity/patient/fragment/RecordFragment.java
@@ -1,13 +1,15 @@
-package com.triadss.doctrack2.activity.healthprof.fragment;
+package com.triadss.doctrack2.activity.patient.fragment;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
 import com.triadss.doctrack2.R;
 
 /**
@@ -25,6 +27,10 @@ public class RecordFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private RecordFragmentPageAdapter pageAdapter;
 
     public RecordFragment() {
         // Required empty public constructor
@@ -50,7 +56,7 @@ public class RecordFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -61,6 +67,44 @@ public class RecordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_record, container, false);
+        tabLayout = rootView.findViewById(R.id.tabLayout);
+        viewPager = rootView.findViewById(R.id.viewPager);
+
+        pageAdapter = new RecordFragmentPageAdapter(getActivity().getSupportFragmentManager(), getLifecycle());
+
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Tab selected logic here
+                if(tab != null)
+                {
+                    viewPager.setCurrentItem(tab.getPosition());
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Tab unselected logic here
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Tab reselected logic here
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public  void onPageSelected(int position)
+            {
+                super.onPageSelected(position);
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+        return rootView;
     }
 }
