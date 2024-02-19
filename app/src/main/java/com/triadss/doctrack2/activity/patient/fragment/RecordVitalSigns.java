44b5e4f7b8ca7f3ee10f6c4c47e80fad3bb716a4
Revya <revya25@gmail.com>
2024-02-19 20:17:17 +0800

    Added contents for Record's Vital Signs and Personal Info

--- app/src/main/java/com/triadss/doctrack2/activity/patient/fragment/RecordVitalSigns.java
+++ app/src/main/java/com/triadss/doctrack2/activity/patient/fragment/RecordVitalSigns.java
@@ -61,6 +61,6 @@ public class RecordVitalSigns extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_vital_signs2, container, false);
+        return inflater.inflate(R.layout.fragment_record_vital_signs, container, false);
     }
 }
