22feb5766d1044192d90b4ef0728717810d496d4
Revya <revya25@gmail.com>
2024-02-26 21:18:30 +0800

    Implemented Medication Base Page Tab switching Slightly modified Add Medication Page

--- app/src/main/java/com/triadss/doctrack2/activity/patient/fragment/PatientMedicationAddFragment.java
+++ app/src/main/java/com/triadss/doctrack2/activity/patient/fragment/PatientMedicationAddFragment.java
@@ -1,4 +1,4 @@
-package com.triadss.doctrack2.activity.healthprof.fragment;
+package com.triadss.doctrack2.activity.patient.fragment;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.triadss.doctrack2.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PatientAddMedication#newInstance} factory method to
+ * Use the {@link PatientMedicationAddFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PatientAddMedication extends Fragment {
+public class PatientMedicationAddFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class PatientAddMedication extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PatientAddMedication() {
+    public PatientMedicationAddFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class PatientAddMedication extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PatientAddMedication.
+     * @return A new instance of fragment PatientMedicationAddFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PatientAddMedication newInstance(String param1, String param2) {
-        PatientAddMedication fragment = new PatientAddMedication();
+    public static PatientMedicationAddFragment newInstance(String param1, String param2) {
+        PatientMedicationAddFragment fragment = new PatientMedicationAddFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
