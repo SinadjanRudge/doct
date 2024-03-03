3957319e3ec20f8a048ec2df89dfe97466385e46
Revya <revya25@gmail.com>
2024-03-03 09:51:51 +0800

    Added Ongoing Medication List Population

--- app/src/main/java/com/triadss/doctrack2/dto/DateTimeDto.java
+++ app/src/main/java/com/triadss/doctrack2/dto/DateTimeDto.java
@@ -2,6 +2,7 @@ package com.triadss.doctrack2.dto;
 
 import com.google.firebase.Timestamp;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -35,4 +36,16 @@ public class DateTimeDto {
                 new Date(date.getYear() - 1900, date.getMonth(), date.getDay(), time.getHour(), time.getMinute()));
     }
 
+    public static DateTimeDto ToDateTimeDto(Timestamp timestamp)
+    {
+        Date date = timestamp.toDate();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        DateTimeDto converted = new DateTimeDto();
+        converted.setDate(new DateDto(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH)));
+        converted.setTime(new TimeDto(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE)));
+
+        return converted;
+    }
 }
