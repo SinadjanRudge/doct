dbc80e72be369184f791c379ad4fff32ba08117c
Mary Ann Inoc <maryanninoc2190@gmail.com>
2024-03-09 09:53:39 +0800

    Item_PatientAppointment_Mary Item_PatientAppointmentReschedule 3-8-2024 12:27

--- app/src/main/java/com/triadss/doctrack2/activity/healthprof/fragment/PatientAppointmentReschedule.java
+++ app/src/main/java/com/triadss/doctrack2/activity/healthprof/fragment/PatientAppointmentReschedule.java
@@ -12,10 +12,10 @@ import com.triadss.doctrack2.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PatientAppointmentReschedule_Mary#newInstance} factory method to
+ * Use the {@link PatientAppointmentReschedule#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PatientAppointmentReschedule_Mary extends Fragment {
+public class PatientAppointmentReschedule extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class PatientAppointmentReschedule_Mary extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PatientAppointmentReschedule_Mary() {
+    public PatientAppointmentReschedule() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class PatientAppointmentReschedule_Mary extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PatientAppointmentReschedule_Mary.
+     * @return A new instance of fragment PatientAppointmentReschedule.
      */
     // TODO: Rename and change types and number of parameters
-    public static PatientAppointmentReschedule_Mary newInstance(String param1, String param2) {
-        PatientAppointmentReschedule_Mary fragment = new PatientAppointmentReschedule_Mary();
+    public static PatientAppointmentReschedule newInstance(String param1, String param2) {
+        PatientAppointmentReschedule fragment = new PatientAppointmentReschedule();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class PatientAppointmentReschedule_Mary extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_appointment_reschedule__mary, container, false);
+        return inflater.inflate(R.layout.fragment_patient_appointment_reschedule, container, false);
     }
 }
