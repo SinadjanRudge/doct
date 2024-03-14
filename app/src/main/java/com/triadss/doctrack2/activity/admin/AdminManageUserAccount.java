b0a60d09968c8f828042b74fc5f631c665b8dbdd
Revya <revya25@gmail.com>
2024-03-15 07:42:37 +0800

    Manange User Accounts navigate to Add Health Professional Profile page

--- app/src/main/java/com/triadss/doctrack2/activity/admin/AdminManageUserAccount.java
+++ app/src/main/java/com/triadss/doctrack2/activity/admin/AdminManageUserAccount.java
@@ -1,17 +1,22 @@
 package com.triadss.doctrack2.activity.admin;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.triadss.doctrack2.R;
+import com.triadss.doctrack2.activity.healthprof.fragment.AddPatientFragment;
 import com.triadss.doctrack2.dto.AddPatientDto;
 import com.triadss.doctrack2.dto.AppointmentDto;
 import com.triadss.doctrack2.dto.HealthProfDto;
@@ -79,6 +84,21 @@ public class AdminManageUserAccount extends Fragment {
 
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_admin_manage_user_account, container, false);
+        Button addHealthProfBtn = rootView.findViewById(R.id.addHealthProfBtn);
+
+        addHealthProfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                @SuppressLint("CommitTransaction")
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager()
+                        .beginTransaction();
+                transaction.replace(R.id.frame_layout, new CreateHealthProfessionalPage());
+                // Add HomeFragment to the back stack with a tag
+                transaction.addToBackStack("tag_for_home_fragment");
+
+                transaction.commit();
+            }
+        });
 
         repository.getHealthProfList(new HealthProfRepository.HealthProListCallback() {
             @Override
