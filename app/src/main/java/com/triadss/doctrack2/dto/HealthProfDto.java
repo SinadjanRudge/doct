989d08849f0137f4452ad68f0b8f1bfe2504ceee
abarquezd3 <abarquezd3@gmail.com>
2024-03-17 14:54:55 +0800

    DA - add health prof (bug)

--- app/src/main/java/com/triadss/doctrack2/dto/HealthProfDto.java
+++ app/src/main/java/com/triadss/doctrack2/dto/HealthProfDto.java
@@ -1,8 +1,45 @@
 package com.triadss.doctrack2.dto;
 
+import com.google.firebase.Timestamp;
+
 public class HealthProfDto {
 
-    private String fullName;
-    public String setFullName(String fullName) { return fullName;}
-    public String getFullName() {return fullName;}
+    private String HWN;
+    private String position;
+    private String userName;
+    private String password;
+    private String healthProfId;
+    private String appointmentId;
+    private String gender;
+    public HealthProfDto()
+    {
+
+    }
+    public HealthProfDto(String HWN, String position, String userName, String password, String appointmentId,
+                         String gender) {
+        this.HWN = HWN;
+        this.position = position;
+        this.userName = userName;
+        this.password = password;
+        this.appointmentId = appointmentId;
+        this.gender = gender;
+    }
+    public void setHealthProfid(String healthProfId) {
+        this.healthProfId = healthProfId;
+    }
+    public String setFullName(String HWN) { return HWN;}
+    public String setPosition(String position) { return position;}
+    public String setUserName(String userName) { return userName;}
+    public String setPassword(String password) { return password;}
+    public String setAppointmentId(String appointmentId) { return appointmentId;}
+    public String setGender(String gender) { return gender;}
+
+    public String getFullName() {return HWN;}
+    public String getHealthProfid() {return healthProfId;}
+    public String getPosition() {return position;}
+    public String getUserName() {return userName;}
+    public String getPassword() {return password;}
+    public String getAppointmentId() {return appointmentId;}
+    public String getGender() {return gender;}
+
 }
