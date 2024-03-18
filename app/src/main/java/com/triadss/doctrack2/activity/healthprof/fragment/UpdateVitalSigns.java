7f07737bfb7c18b30ed08d8b75055f016cc90a74
jcano <jcano@csyoutsoutcing.com>
2024-03-18 13:56:30 +0800

    Added Fragment classes for Updating Patient Records

---
+++ app/src/main/java/com/triadss/doctrack2/activity/healthprof/fragment/UpdateVitalSigns.java
@@ -0,0 +1,76 @@
+package com.triadss.doctrack2.activity.healthprof.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.triadss.doctrack2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UpdateVitalSigns#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UpdateVitalSigns extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public UpdateVitalSigns() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment addMedicalRecord.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static UpdateVitalSigns newInstance(String param1, String param2) {
+        UpdateVitalSigns fragment = new UpdateVitalSigns();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_update_vital_signs, container, false);
+        Button submit = rootView.findViewById(R.id.submitBtn);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do Action here
+            }
+        });
+
+        return rootView;
+    }
+}
