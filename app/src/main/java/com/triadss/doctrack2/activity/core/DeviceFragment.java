f2051c7f36ff3a6da20a97bdf3e8ec20c1ca05a2
Revya <revya25@gmail.com>
2024-03-20 21:23:06 +0800

    Prepared basic sender for device fragment

--- app/src/main/java/com/triadss/doctrack2/activity/core/DeviceFragment.java
+++ app/src/main/java/com/triadss/doctrack2/activity/core/DeviceFragment.java
@@ -1,22 +1,42 @@
 package com.triadss.doctrack2.activity.core;
 
+import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.wearable.MessageClient;
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
 import com.triadss.doctrack2.R;
+import com.triadss.doctrack2.activity.MainActivity;
+import com.triadss.doctrack2.dto.BluetoothDeviceDto;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class DeviceFragment extends Fragment implements MessageClient.OnMessageReceivedListener {
 
     private TextView receivedMessageTextView;
-    private MessageClient messageClient;
     private String phoneNodeId = "565df7c9"; // Declare phoneNodeId at the class level
+
+    // NEW CODE
+    private Handler handler;
 
     public DeviceFragment() {
         // Required empty public constructor
@@ -29,13 +49,27 @@ public class DeviceFragment extends Fragment implements MessageClient.OnMessageR
         View rootView = inflater.inflate(R.layout.fragment_device_with_device, container, false);
 
         receivedMessageTextView = rootView.findViewById(R.id.receivedMessageTextView);
+        Button sendMessage = rootView.findViewById(R.id.sendMessageBtn);
 
-        // Initialize MessageClient
-        messageClient = Wearable.getMessageClient(requireContext());
-        messageClient.addListener(this);
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Bundle stuff = msg.getData();
+                String logText = "\nRetrieved message: " + stuff.getString(MessageKey);
+                receivedMessageTextView.append("\n" + logText);
+                Log.e(TAG, logText);
+                return true;
+            }
+        });
 
-        // Get the phoneNodeId (Wear OS device ID)
-        phoneNodeId = getPhoneNodeId();
+        // Send sample message on click
+        sendMessage.setOnClickListener(v -> {
+            String onClickMessage = "I just sent the handheld a message ";
+            Log.e(TAG, "Send message: " + onClickMessage);
+
+            //Use the same path//
+            new SenderThread(DataPath, onClickMessage).start();
+        });
 
         return rootView;
     }
@@ -43,10 +77,7 @@ public class DeviceFragment extends Fragment implements MessageClient.OnMessageR
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        // Remove the message listener
-        messageClient.removeListener(this);
     }
-
 
 
     @Override
@@ -80,6 +111,66 @@ public class DeviceFragment extends Fragment implements MessageClient.OnMessageR
         } else {
             Log.e("DeviceFragment", "Phone node ID not available");
             return null;
+        }
+    }
+
+
+    // NEW CODE
+    private static final String TAG = "DEVICE";
+    private static final String MessageKey = "MessageText";
+    private static final String DataPath = "/my_path";
+    private int sentMessageCounter = 1;
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        // ON RECEIVE FUNCTION HERE
+        public void onReceive(Context context, Intent intent) {
+            Log.e(TAG, "Received message from wearable");
+        }
+    };
+
+    private void sendMessage(String messageText)
+    {
+        Bundle bundle = new Bundle();
+        bundle.putString(MessageKey, messageText);
+        Message msg = handler.obtainMessage();
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+    }
+
+    private class SenderThread extends Thread
+    {
+        String path;
+        String message;
+        //Constructor for sending information to the Data Layer//
+        SenderThread(String p, String m) {
+            path = p;
+            message = m;
+        }
+        public void run() {
+            //Retrieve the connected devices, known as nodes//
+            Task<List<Node>> wearableList =
+                    Wearable.getNodeClient(getContext()).getConnectedNodes();
+            try {
+                List<Node> nodes = Tasks.await(wearableList);
+                for (Node node : nodes) {
+                    Task<Integer> sendMessageTask =
+                    //Send the message//
+                    Wearable.getMessageClient(getActivity()).sendMessage(node.getId(), path, message.getBytes());
+                    try {
+                        //Block on a task and get the result synchronously//
+                        Integer result = Tasks.await(sendMessageTask);
+                        sendMessage("Sent Sample Message " + sentMessageCounter++);
+                        //if the Task fails, then…..//
+                    } catch (ExecutionException exception) {
+                        //TO DO: Handle the exception//
+                    } catch (InterruptedException exception) {
+                        //TO DO: Handle the exception//
+                    }
+                }
+            } catch (ExecutionException exception) {
+                //TO DO: Handle the exception//
+            } catch (InterruptedException exception) {
+                //TO DO: Handle the exception//
+            }
         }
     }
 }
