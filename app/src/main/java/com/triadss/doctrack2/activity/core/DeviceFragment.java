3b209148fb96ed7025e4a6d791a02c26bc75ca14
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-03-30 11:36:55 +0800

    added additional labels in the device registered

--- app/src/main/java/com/triadss/doctrack2/activity/core/DeviceFragment.java
+++ app/src/main/java/com/triadss/doctrack2/activity/core/DeviceFragment.java
@@ -1,6 +1,10 @@
 package com.triadss.doctrack2.activity.core;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -36,9 +40,11 @@ import com.triadss.doctrack2.R;
 import com.triadss.doctrack2.bluetooth.MessageService;
 import com.triadss.doctrack2.config.constants.BluetoothConstants;
 import com.triadss.doctrack2.dto.VitalSignsDto;
+import com.triadss.doctrack2.dto.WearableDeviceDto;
 import com.triadss.doctrack2.repoositories.VitalSignsRepository;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 public class DeviceFragment extends Fragment {
@@ -51,12 +57,19 @@ public class DeviceFragment extends Fragment {
     private Receiver messageReceiver;
     private int count = 0;
     private VitalSignsRepository vitalSignsRepo = new VitalSignsRepository();
+    private WearableDeviceDto wearableDeviceDto = new WearableDeviceDto();
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     private FirebaseUser user = auth.getCurrentUser();
     VitalSignsDto vitalSignsDto = new VitalSignsDto();
 
+    //* TextViews for the Vital Signs
     private TextView bloodPressureValue, temperatureValue, spo2Value, pulseRateValue, weightValue, heightValue, BMIValue;
 
+    //* TextViews for the Device Registered
+//    private TextView
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    private static final long CHECK_INTERVAL_MILLISECONDS = 5000;
+    private static boolean checkOnce = false;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_device_with_device, container, false);
@@ -64,13 +77,78 @@ public class DeviceFragment extends Fragment {
         initializeHandlers();
         initializeListeners();
         initVitalSigns();
+//        checkPairedDevices();
+//        startContinuousCheck();
         return rootView;
+    }
+
+    private void startContinuousCheck() {
+        new Thread(() -> {
+            while (true) {
+                checkIfPairedDevice();
+                try {
+                    Thread.sleep(CHECK_INTERVAL_MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void checkIfPairedDevice() {
+        new Thread(() -> {
+            Context context = requireContext();
+            Task<List<Node>> nodeListTask = Wearable.getNodeClient(context).getConnectedNodes();
+            try {
+                List<Node> nodes = Tasks.await(nodeListTask);
+                for (Node node : nodes) {
+                    String nodeName = node.getDisplayName();
+                    String nodeId = node.getId();
+                    boolean isNearby = node.isNearby();
+
+                    Log.d(TAG, "Node Name: " + nodeName);
+                    Log.d(TAG, "Node ID: " + nodeId);
+                    Log.d(TAG, "Is Nearby: " + isNearby);
+
+                    wearableDeviceDto.setDeviceId(nodeId);
+                    wearableDeviceDto.setDeviceName(nodeName);
+                    wearableDeviceDto.setIsNearby(isNearby);
+                }
+
+                showPairedDeviceStatus(nodes.size() == 1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+
+    private void showPairedDeviceStatus(boolean isPaired) {
+        requireActivity().runOnUiThread(() -> {
+            if (isPaired) {
+                //
+                // Paired device connected
+                if(!checkOnce) {
+                    Toast.makeText(requireContext(), "Paired with a smartwatch", Toast.LENGTH_SHORT).show();
+                    checkOnce = true;
+                }
+
+            } else {
+                // No paired device connected
+                if(checkOnce) {
+                    Toast.makeText(requireContext(), "Please pair a smartwatch", Toast.LENGTH_SHORT).show();
+                    checkOnce = false;
+                }
+            }
+        });
     }
 
     private void initializeViews(View rootView) {
         receivedMessageTextView = rootView.findViewById(R.id.receivedMessageTextView);
         sendMessageBtn = rootView.findViewById(R.id.sendMessageBtn);
         syncButton = rootView.findViewById(R.id.SyncDeviceBtn);
+
+        //* Vital Signs Views
         bloodPressureValue = rootView.findViewById(R.id.BloodPressureValue);
         temperatureValue = rootView.findViewById(R.id.TemperatureValue);
         spo2Value = rootView.findViewById(R.id.Spo2Value);
@@ -78,6 +156,8 @@ public class DeviceFragment extends Fragment {
         weightValue = rootView.findViewById(R.id.WeightValue);
         heightValue = rootView.findViewById(R.id.HeightValue);
         BMIValue = rootView.findViewById(R.id.BMIValue);
+
+
     }
 
     private void initializeHandlers() {
@@ -129,6 +209,63 @@ public class DeviceFragment extends Fragment {
         heightValue.setText(vitalSigns.getHeight() + "");
         BMIValue.setText(vitalSigns.getBMI() + "");
     }
+
+    private void setDeviceRegistered(WearableDeviceDto wearableDeviceDto){
+
+    }
+
+
+    private void checkPairedDevices() {
+        // Get the BluetoothAdapter
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Bluetooth is not supported on this device
+            Toast.makeText(getContext(), "Bluetooth is not supported", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check if Bluetooth is enabled
+        if (!bluetoothAdapter.isEnabled()) {
+            // Bluetooth is not enabled, prompt the user to enable it
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH);
+            return;
+        }
+
+        // Get the list of paired devices
+        @SuppressLint("MissingPermission") Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (((Set<?>) pairedDevices).size() > 0) {
+            // There are paired devices, display their information
+            StringBuilder devicesInfo = new StringBuilder();
+            for (BluetoothDevice device : pairedDevices) {
+                @SuppressLint("MissingPermission") String deviceInfo = "Name: " + device.getName() + "\n"
+                        + "Address: " + device.getAddress() + "\n"
+                        + "Type: " + getDeviceType(device) + "\n\n";
+                devicesInfo.append(deviceInfo);
+            }
+            // Display the paired devices information in a TextView
+            receivedMessageTextView.setText(devicesInfo.toString());
+        } else {
+            // No paired devices found
+            receivedMessageTextView.setText("No paired devices found");
+        }
+    }
+
+    private String getDeviceType(BluetoothDevice device) {
+        @SuppressLint("MissingPermission") int deviceType = device.getType();
+        switch (deviceType) {
+            case BluetoothDevice.DEVICE_TYPE_CLASSIC:
+                return "Classic";
+            case BluetoothDevice.DEVICE_TYPE_LE:
+                return "Low Energy (LE)";
+            case BluetoothDevice.DEVICE_TYPE_DUAL:
+                return "Dual Mode";
+            case BluetoothDevice.DEVICE_TYPE_UNKNOWN:
+            default:
+                return "Unknown";
+        }
+    }
+
 
     private void handleSyncButtonClick() {
         try {
