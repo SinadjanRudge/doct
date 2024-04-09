fb550decc1bb622447733f77165454041dd221c2
Revya <revya25@gmail.com>
2024-04-09 21:26:54 +0800

    Updated gradle lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-
+    implementation("androidx.work:work-runtime:2.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
