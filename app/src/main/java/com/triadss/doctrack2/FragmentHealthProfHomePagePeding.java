aa01187b40fd918662fc7ff91c64eae2a23945dd
Mary Ann Inoc <maryanninoc2190@gmail.com>
2024-04-13 12:31:28 +0800

    fragment health home page pending

--- app/src/main/java/com/triadss/doctrack2/FragmentHealthProfHomePagePeding.java
+++ app/src/main/java/com/triadss/doctrack2/FragmentHealthProfHomePagePeding.java
@@ -59,6 +59,6 @@ public class FragmentHealthProfHomePagePeding extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_health_prof_home_page_peding, container, false);
+        return inflater.inflate(R.layout.fragment_health_prof_home_page_pending, container, false);
     }
 }
