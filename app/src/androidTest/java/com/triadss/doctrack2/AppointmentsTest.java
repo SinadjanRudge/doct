e315482d66d1d3d49db6599ae7e659769d4e3b69
muniing <placenciamitel14@gmail.com>
2024-05-17 00:52:39 +0800

    Testing

--- app/src/androidTest/java/com/triadss/doctrack2/AppointmentsTest.java
+++ app/src/androidTest/java/com/triadss/doctrack2/AppointmentsTest.java
@@ -20,8 +20,10 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import com.triadss.doctrack2.activity.MainActivity;
 import com.triadss.doctrack2.activity.healthprof.HealthProfHome;
 import com.triadss.doctrack2.activity.patient.PatientHome;
+import com.triadss.doctrack2.dto.DateTimeDto;
 
 import org.junit.After;
 import org.junit.Before;
@@ -29,12 +31,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Calendar;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class AppointmentsTest {
+    DateTimeDto PickerActions = null;
     @Rule
-    public ActivityScenarioRule<HealthProfHome> activityRule =
-            new ActivityScenarioRule<HealthProfHome>(HealthProfHome.class);
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<MainActivity>(MainActivity.class);
     @Before
     public void setup() {
         Intents.init();
@@ -45,90 +50,88 @@ public class AppointmentsTest {
         Intents.release();
     }
     public void login(){
-        onView(withId(R.id.email)).perform(typeText("testpatient198@gmail.com"));
-        onView(withId(R.id.password)).perform((typeText("123456")), closeSoftKeyboard());
+        onView(withId(R.id.email)).perform(typeText("testhealthprof@gmail.com"));
+        onView(withId(R.id.password)).perform((typeText("test@123456")), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
         SystemClock.sleep(5000);
     }
-    //@Test
+    @Test
     public void requestAppointment(){
-        login();
         onView(withId(R.id.appointment_menu)).perform(click());
-        SystemClock.sleep(5000);
         onView(withId(R.id.textInputPurpose)).perform(typeText("For Checkup"));
         onView(withId(R.id.datebutton)).perform(click());
         SystemClock.sleep(5000);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        onView(withId(android.R.id.button1)).perform(PickerActions.setDate(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH) + 1,
+                calendar.get(Calendar.DAY_OF_MONTH)));
+        onView(withText("OK")).perform(click());
         onView(withId(R.id.idBtnPickTime)).perform(click());
-        SystemClock.sleep(5000);
+        onView(withText("9:00 am - 10:00 am")).perform(click());
         onView(withId(R.id.confirmbutton)).perform(click());
-
+        onView(withText("Requested")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
-    //@Test
-//    public void rescheduleAppointment(){
-//        onView(withId(R.id.email)).perform(typeText("testhealthprof@gmail.com"));
-//        onView(withId(R.id.password)).perform((typeText("test@123456")), closeSoftKeyboard());
-//        onView(withId(R.id.btn_login)).perform(click());
-//        SystemClock.sleep(5000);
-//        onView(withId(R.id.appointment_menu)).perform(click());
-//        SystemClock.sleep(5000);
-//        onView(withId(R.id.fragment_health_professional_upcoming)).check(matches(isDisplayed()));
-//        onView(withId(R.id.accept_button)).perform(click());
-//    }
-    //@Test
-    public void rejectAppointment(){
-        SystemClock.sleep(5000);
+    @Test
+    public void acceptAppointment(){
         onView(withId(R.id.appointment_menu)).perform(click());
         SystemClock.sleep(5000);
+        onView(withId(R.id.fragment_health_professional_upcoming)).check(matches(isDisplayed()));
+        onView(withId(R.id.accept_button)).perform(click());
+        onView(withText("Accepted")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+    @Test
+    public void rejectAppointment(){
         onView(withId(R.id.fragment_health_professional_upcoming)).check(matches(isDisplayed()));
         onView(withId(R.id.reject_button)).perform(click());
+        onView(withText("Rejected")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
-    //@Test
+    @Test
     public void viewUpcomingAppointments(){
-        SystemClock.sleep(5000);
         onView(withId(R.id.appointment_menu)).perform(click());
         SystemClock.sleep(5000);
         onView(withId(R.id.fragment_health_professional_upcoming)).check(matches(isDisplayed()));
     }
-//    @Test
+    @Test
     public void viewPendingAppointments(){
-        SystemClock.sleep(5000);
-        onView(withId(R.id.appointment_menu)).perform(click());
+        onView(withId(R.id.fragment_health_professional_upcoming)).check(matches(isDisplayed()));
         SystemClock.sleep(5000);
         onView(withId(R.id.fragment_health_professional_upcoming)).perform(swipeLeft());
         SystemClock.sleep(5000);
         onView(withId(R.id.fragment_health_professional_pending)).check(matches(isDisplayed()));
     }
 
-    //@Test
+    @Test
     public void cancelPendingAppointment(){
-        onView(withId(R.id.email)).perform(typeText("testhealthprof@gmail.com"));
-        onView(withId(R.id.password)).perform((typeText("test@123456")), closeSoftKeyboard());
-        onView(withId(R.id.btn_login)).perform(click());
-        SystemClock.sleep(5000);
         onView(withId(R.id.appointment_menu)).perform(click());
         SystemClock.sleep(5000);
         onView(withId(R.id.fragment_health_professional_upcoming)).perform(swipeLeft());
         SystemClock.sleep(5000);
-        //onView(withId(R.id.btn_cancel)).perform(click());
+        onView(withId(R.id.btn_cancel)).perform(click());
+        onView(withText("Cancelled")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
-    //@Test
+    @Test
     public void rescheduleAppointment(){
-        onView(withId(R.id.email)).perform(typeText("testhealthprof@gmail.com"));
-        onView(withId(R.id.password)).perform((typeText("test@123456")), closeSoftKeyboard());
-        onView(withId(R.id.btn_login)).perform(click());
-        SystemClock.sleep(5000);
         onView(withId(R.id.appointment_menu)).perform(click());
         SystemClock.sleep(10000);
         onView(withId(R.id.updateDate)).perform(click());
         SystemClock.sleep(5000);
-        onView(withId(R.id.updateTime)).perform(click());
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        onView(withId(android.R.id.button1)).perform(PickerActions.setDate(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH) + 1,
+                calendar.get(Calendar.DAY_OF_MONTH)));
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.updateDate)).perform(click());
+        onView(withText("10:00 am - 11:00 am")).perform(click());
         SystemClock.sleep(5000);
         onView(withId(R.id.confirmbutton));
+        onView(withText("Rescheduled")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
     @Test
     public void viewAppointmentStatus(){
-        login();
-        SystemClock.sleep(5000);
         onView(withId(R.id.appointment_menu)).perform(click());
         SystemClock.sleep(5000);
         onView(withId(R.id.activity_patient_request)).perform(swipeLeft());
